5a85352b56d42909c8e2152196b5c25eb3b5506d
Julio-Maranhao <122162004+Julio-Maranhao@users.noreply.github.com>
2023-03-18 20:42:29 -0300

    Update apps.py

--- filme/apps.py
+++ filme/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
+
+    def ready(self):  # Para identificar o usuário e a senha do ADMIN no ambiente
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")  # Crie seu email no ambiente
+        senha = os.getenv("SENHA_ADMIN")  # Crie sua senha no ambiente
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
